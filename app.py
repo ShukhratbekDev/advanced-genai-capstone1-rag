f97137fa5c8d1be2561faa62c359b102c756aca7
ShukhratbekDev <shukhratbek.dev@gmail.com>
2026-01-30 01:06:21 +0500

    fix: add pysqlite3 for compatibility and update deps

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import gradio as gr
 import os
 from rag_engine import RAGHelper
