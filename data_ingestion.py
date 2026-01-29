6f2d203f2494bc346ee702e26c1ab13d56eeada5
ShukhratbekDev <shukhratbek.dev@gmail.com>
2026-01-30 01:08:35 +0500

    fix: update text splitter imports and add langchain-text-splitters

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,10 +2,10 @@ import os
 import glob
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.schema import Document
+from langchain_core.documents import Document
 from dotenv import load_dotenv
 
 load_dotenv()
